e559ccf022b7b3a30b0379ba24eb52f307ce7802
Naveen M K <naveen@syrusdark.website>
2021-05-14 01:55:35 +0530

    WIP: Some style changes

--- frontend/index.js
+++ frontend/index.js
@@ -1,27 +1,31 @@
-import './style.scss';
-import 'bootstrap';
-import tippy from 'tippy.js';
+import "./style.scss";
+import "bootstrap";
+import tippy from "tippy.js";
 
 class App {
-
     static copyToClipboard(button) {
-        let text = button.parentNode.getElementsByTagName("code")[0].innerText
+        let text = button.parentNode.getElementsByTagName("code")[0].innerText;
         if (navigator.clipboard) {
             navigator.clipboard.writeText(text);
             let oldInner = button.innerHTML;
             button.innerHTML = "✅";
-            setTimeout(() => button.innerHTML = oldInner, 1000);
+            setTimeout(() => (button.innerHTML = oldInner), 1000);
         }
     }
+}
 
-};
-
-tippy('.mytooltip', {
+tippy(".mytooltip", {
     allowHTML: true,
-    theme: 'light-border',
+    theme: "light-border",
     content(reference) {
         return reference.querySelector(".mytooltip-content").innerHTML;
     },
 });
 
+function seachBoxUpdate(){
+    search = document.getElementById('navbar-search')
+    search.style
+    
+}
+seachBoxUpdate()
 window.App = App;
